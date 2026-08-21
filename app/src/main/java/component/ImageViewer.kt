9767db174311e60f5c9994dc92875d027ea4b75d
qjw02 <93773921+qjw02@users.noreply.github.com>
2026-08-21 14:15:32 +0800

    perf: load thread images at display size

--- app/src/main/java/component/ImageViewer.kt
+++ app/src/main/java/component/ImageViewer.kt
@@ -1,119 +1,49 @@
 package com.qjw.forum.component
 
-
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
-
-
+import coil.compose.AsyncImage
 
 @Composable
-fun ImageViewer(
+fun ImageViewer(url: String) {
+    var show by remember { mutableStateOf(false) }
 
-    url:String
-
-){
-
-
-    var show by remember {
-
-        mutableStateOf(false)
-
-    }
-
-
-
-    Image(
-
-        painter =
-            rememberAsyncImagePainter(url),
-
-
-        contentDescription=null,
-
-
-        contentScale =
-            ContentScale.Crop,
-
-
-        modifier =
-
-            Modifier
-
-                .fillMaxWidth()
-
-                .height(250.dp)
-
-                .clickable {
-
-                    show=true
-
-                }
-
+    AsyncImage(
+        model = url,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
+            .clickable { show = true }
     )
 
-
-
-
-
-    if(show){
-
-
-
-        androidx.compose.foundation.layout.Box(
-
-            modifier =
-
-                Modifier
-
-                    .fillMaxSize()
-
-                    .background(Color.Black)
-
-                    .clickable {
-
-                        show=false
-
-                    }
-
-        ){
-
-
-
-            Image(
-
-                painter =
-                    rememberAsyncImagePainter(url),
-
-
-                contentDescription=null,
-
-
-                contentScale =
-                    ContentScale.Fit,
-
-
-                modifier =
-                    Modifier.fillMaxSize()
-
-
+    if (show) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+                .clickable { show = false }
+        ) {
+            AsyncImage(
+                model = url,
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.fillMaxSize()
             )
-
-
         }
-
-
     }
-
-
-
 }
