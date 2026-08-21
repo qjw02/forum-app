62a8f431b8075c055c6fa70e95d73b26de80f302
qjw02 <93773921+qjw02@users.noreply.github.com>
2026-08-21 13:57:07 +0800

    fix: support GIF homepage banner ads

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("io.coil-kt:coil-gif:2.6.0")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
