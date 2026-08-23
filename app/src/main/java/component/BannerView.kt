e94897e07834923d360bf04333a79e2162f9acfa
qjw02 <93773921+qjw02@users.noreply.github.com>
2026-08-24 00:03:38 +0800

    Fix BannerView Kotlin syntax and Compose compatibility

--- app/src/main/java/component/BannerView.kt
+++ app/src/main/java/component/BannerView.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
